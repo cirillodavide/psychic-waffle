7dfa996a071c84823ce5e12fab2f65ab46b43b60
Davide Cirillo <cirillod@gmail.com>
2017-02-10 12:18:35 +0100

    a more sophisticated interface

--- ui.r
+++ ui.r
@@ -7,7 +7,7 @@ shinyUI(pageWithSidebar(
     downloadButton('downloadData', 'Download'),
     uiOutput("choose_columns"),
     br(),
-    a(href = "https://gist.github.com/4211337", "Source code")
+    a(href = "https://github.com/cirillodavide/psychic-waffle", "Source code")
   ),
 
   mainPanel(
@@ -22,19 +22,21 @@ shinyUI(pageWithSidebar(
                   max = 10,
                   value = 1),
     sliderInput("span",
-                  "Minimum span",
+                  "Span cutoff",
                   min = 0,
                   max = 10,
                   value = 0),
       fluidRow(
-        splitLayout(cellWidths = c("70%", "30%"), plotOutput("distPlot1"), column(2,tableOutput('table')))
+        column(6, plotOutput("x2")),
+        column(6, DT::dataTableOutput('x1'))
       ),
     sliderInput("range",
                   "Time range",
-                  min = 0,
-                  max = 100,
                   value = c(0,10),
-                  round = TRUE),
-    plotOutput("distPlot2")
+                  min = 0,
+                  max = 60, 
+                  step = 0.01,
+                  round = FALSE),
+    plotOutput("x3")
   )
 ))
